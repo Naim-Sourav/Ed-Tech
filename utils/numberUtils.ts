03999960b56e7eb58fc3fbb40cd6fd3aa9156881
Nur Naim Islam Sourav <221029.pharm@student.just.edu.bd>
2026-04-12 16:03:50 +0600

    Add files via upload

---
+++ utils/numberUtils.ts
@@ -0,0 +1,6 @@
+
+export const toBengaliNumber = (num: number | string | undefined | null): string => {
+  if (num === undefined || num === null) return '';
+  const bengaliDigits = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
+  return num.toString().replace(/\d/g, (digit) => bengaliDigits[parseInt(digit)]);
+};
