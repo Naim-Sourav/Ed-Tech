7d4259fb75db9ce488275434f78a140ca00349d8
Nur Naim Islam Sourav <221029.pharm@student.just.edu.bd>
2026-06-20 08:34:28 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,18 @@ export default {
         tiro: ['Kalpurush', 'sans-serif'],
         display: ['Outfit', 'Inter', 'sans-serif'],
       },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: ['1rem', { lineHeight: '1.6', fontWeight: '400' }],
+        md: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
